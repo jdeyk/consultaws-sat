62b284e56fa3db3b547c9d14c2b368c57b26b372
Jose Rafael <joserra_m26@live.com>
2021-11-25 12:21:12 -0600

    clase servicio sat

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+class ServiceSAT
+{
+
+    function __constructor(){
+
+    }
+    
+    public function getStatus($file_xml){
+
+    }
+}
+
+
+$service = new ServiceSAT();
+$result = $service->getStatus('...');
+
+var_dump($result);
